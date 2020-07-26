e187299377207c1dea9a5f5f67027e06983946fc
skhabeeb42 <skhabeeb42@gmail.com>
2020-07-26 14:40:16 +0530

    referralCode added in model

--- server.js
+++ server.js
@@ -23,12 +23,6 @@ app.use((err, req, res, next) => {
   return res.status(500).json(err)
 })
 
-app.use(function(req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*")
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
-	next()
-})
-
 module.exports = app
 
 // set mongoose Promise to Bluebird
--- src/api/models/user.model.js
+++ src/api/models/user.model.js
@@ -17,6 +17,10 @@ const userSchema = new mongoose.Schema({
   	type: String,
   	default:""
   },
+  referralCode : {
+  	type: String,
+  	default:""
+  },
   password: {
     type: String,
     minlength: 6
